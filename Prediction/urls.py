ad776f8938a75540dbfebea057e6b385e55476d3
Kuldeep Padhiyar <padhiyarkuldeep22222@gmail.com>
2021-07-25 21:31:42 -0400

    Routing Updated

--- Prediction/urls.py
+++ Prediction/urls.py
@@ -5,4 +5,9 @@ app_name = 'smp'
 urlpatterns = [
     path('', views.index, name="index"),
     path('google', views.google, name="google"),
+    path('twitter', views.twitter, name="twitter"),
+    path('apple', views.apple, name="apple"),
+    path('microsoft', views.microsoft, name="microsoft"),
+    path('getDataFromWeb', views.getDataFromWeb, name="getdataFromWeb"),
+    path('createModel', views.createModel, name="createModel"),
 ]
